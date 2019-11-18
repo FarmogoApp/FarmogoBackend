be7cc09dd6c5195b945a1edef09a32a048884b17
josemanuel97 <manoloainsa@gmail.com>
2019-11-18 16:54:14 +0100

    Animaltype REST

--- Rest/src/main/java/com/farmogo/rest/AnimalTypesRS.java
+++ Rest/src/main/java/com/farmogo/rest/AnimalTypesRS.java
@@ -2,12 +2,11 @@ package com.farmogo.rest;
 
 import com.farmogo.services.AnimalTypesService;
 import com.farmogo.model.AnimalType;
+import com.sun.tracing.dtrace.ProviderAttributes;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -22,4 +21,21 @@ public class AnimalTypesRS {
     public List<AnimalType> getAll() {
         return animalTypesService.getAll();
     }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public AnimalType get(@PathParam("id") String id) {
+        return animalTypesService.get(id);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void save(AnimalType animalType) { animalTypesService.save(animalType);}
+
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void delete(AnimalType animalType) { animalTypesService.delete(animalType);}
+
+
 }
