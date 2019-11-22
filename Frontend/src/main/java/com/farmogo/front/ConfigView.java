4850e501cb3b2a39b1276be8094090de9a3e8229
josemanuel97 <manoloainsa@gmail.com>
2019-11-22 18:11:28 +0100

    Races REST

--- Frontend/src/main/java/com/farmogo/front/ConfigView.java
+++ Frontend/src/main/java/com/farmogo/front/ConfigView.java
@@ -1,9 +1,13 @@
 package com.farmogo.front;
 
 import com.farmogo.model.AnimalCounter;
+import com.farmogo.model.Building;
+import com.farmogo.model.Division;
 import com.farmogo.model.Farm;
+import com.farmogo.services.BuildingService;
 import com.farmogo.services.FarmService;
 import com.farmogo.services.GlobalSessionService;
+import com.sun.corba.se.spi.ior.ObjectId;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -11,7 +15,10 @@ import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.swing.tree.TreeNode;
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
 
 @Named
 @ViewScoped
@@ -20,11 +27,34 @@ public class ConfigView implements Serializable {
     @Inject
     FarmService farmService;
 
+    @Inject
+    BuildingService buildingService;
+
     private Farm farm;
+    private List<Building> buildingsList;
+    private Building building;
 
     @PostConstruct
     public void init(){
         farm = farmService.getCurrentFarm();
+        buildingsList = farm.getBuildings();
+        building = buildingsList.get(0);
+    }
+
+    public void clearBuildingSelection(){
+        building = new Building();
+    }
+
+    public Building getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(Building building){
+        this.building = building;
+    }
+
+    public List<Building> getBuildings(){
+        return buildingsList;
     }
 
     public AnimalCounter getAnimalCounter() {
@@ -33,6 +63,25 @@ public class ConfigView implements Serializable {
 
     public void setAnimalCounter(AnimalCounter animalCounter) {
         this.farm.setAnimalCounter(animalCounter);
+    }
+
+    public Farm getFarm(){
+        return farm;
+    }
+
+    public void saveBuilding(){
+        //this.building.setUuid("8888");
+        Division d11 = new Division();
+        d11.setName("division 1.1");
+        //d11.setUuid("5555");
+
+        this.building.setDivisions(Arrays.asList(d11));
+
+        buildingsList.add(this.building);
+        farm.setBuildings(buildingsList);
+        farmService.save(farm);
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage("Successful", farm.getAnimalCounter().toString()) );
     }
 
     public void save(){
