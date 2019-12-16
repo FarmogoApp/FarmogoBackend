8e1edea6e656cfa5705cb395814daf10b6d87ed1
Sergi Trujillo <sergi.trujillo@gmail.com>
2019-12-16 20:18:13 +0100

    Go back to get all animals including discharged because we need to link its information

--- Rest/src/main/java/com/farmogo/rest/AnimalRS.java
+++ Rest/src/main/java/com/farmogo/rest/AnimalRS.java
@@ -27,7 +27,7 @@ public class AnimalRS {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Animal> getAll() {
-        return animalService.getCurrentAnimals();
+        return animalService.getAll();
     }
 
     @GET
